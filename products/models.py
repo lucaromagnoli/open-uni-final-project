96dad7c174e52a40b2196fda19dc19af1f3a01d2
Luca Romagnoli <romagnoli.luca@gmail.com>
2020-01-19 17:07:18 +0000

    Add city and country to manufacturer

--- products/models.py
+++ products/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Manufacturer(models.Model):
     name = models.CharField(max_length=200)
     website = models.URLField()
+    city = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
