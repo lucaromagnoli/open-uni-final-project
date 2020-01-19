e0a00eda9cac6c00381ce6669a382a1d019cd475
Luca Romagnoli <romagnoli.luca@gmail.com>
2020-01-19 20:29:22 +0000

    City and country can be null

--- products/models.py
+++ products/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class Manufacturer(models.Model):
     name = models.CharField(max_length=200)
     website = models.URLField()
-    city = models.CharField(max_length=200)
-    country = models.CharField(max_length=200)
+    city = models.CharField(max_length=200, blank=True)
+    country = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
         return self.name
