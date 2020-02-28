6e7e911abba14dc4bdf3832c42984513689ecb1a
Luca Romagnoli <luca.romagnoli@sainsburys.co.uk>
2020-02-28 19:46:00 +0000

    add ProductAdmin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Manufacturer
+
+
+class ProductAdmin(admin.ModelAdmin):
+    fields = (
+        'title',
+        'product_images',
+        'gender',
+        'category',
+        'type',
+        'color',
+        'design_details',
+        'materials'
+    )
+    readonly_fields = ('title', 'product_images')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Manufacturer)
