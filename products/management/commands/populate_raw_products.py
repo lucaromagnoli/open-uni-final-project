80fb7c2e14d3fa2e3c7c040628033296a6e2b339
Luca Romagnoli <luca.romagnoli@sainsburys.co.uk>
2020-03-01 23:25:15 +0000

    various changes

--- products/management/commands/populate_raw_products.py
+++ products/management/commands/populate_raw_products.py
@@ -33,7 +33,8 @@ class Command(BaseCommand):
         parser.add_argument(
             '--category',
             help='The category of the products',
-            required=True
+            required=False,
+            default=None
         )
 
     @staticmethod
@@ -54,7 +55,7 @@ class Command(BaseCommand):
         for m in materials.split(','):
             try:
                 mat_objs.append(
-                    models.CategoryMaterial.objects.get(category_pk=category.id, material__name=m)
+                    models.CategoryMaterial.objects.get(category_id=category.id, material__name=m)
                 )
             except ObjectDoesNotExist:
                 continue
@@ -66,7 +67,7 @@ class Command(BaseCommand):
         for d in design_details.split(','):
             try:
                 dd_objs.append(
-                    models.CategoryDesignDetail.objects.get(category_pk=category.id, design_detail__name=d)
+                    models.CategoryDesignDetail.objects.get(category_id=category.id, design_detail__name=d)
                 )
             except ObjectDoesNotExist:
                 continue
@@ -75,7 +76,7 @@ class Command(BaseCommand):
     @staticmethod
     def get_type(category, ptype):
         try:
-            return models.CategoryType.objects.get(category_pk=category.id, type__name=ptype)
+            return models.CategoryType.objects.get(category_id=category.id, type__name=ptype)
         except ObjectDoesNotExist:
             return None
 
@@ -89,22 +90,26 @@ class Command(BaseCommand):
     @staticmethod
     def get_manufacturer(manuf_name, website):
         try:
+            return models.Manufacturer.objects.get(name=manuf_name)
+        except ObjectDoesNotExist:
             manufacturer = models.Manufacturer(name=manuf_name, website=website)
             manufacturer.save()
             return manufacturer
-        except IntegrityError:
-            return models.Manufacturer.objects.get(name=manuf_name, website=website)
 
     @transaction.atomic
     def _populate(self, df, manufacturer, cat_name):
-        category = models.Category.objects.get(name=cat_name)
         for row in df.itertuples():
+            if row.category:
+                category = models.Category.objects.get(name=row.category)
+            else:
+                category = models.Category.objects.get(name=cat_name)
             product = models.Product(
                 title=row.name,
                 category=category,
                 product_url=row.url,
                 manufacturer=manufacturer,
                 gender=self.gender_lookup.get(row.gender),
+                type=self.get_type(category, row.type),
                 description=row.description if row.description else '',
                 currency=row.currency if row.currency else '',
                 price=row.price if row.price else None,
@@ -115,7 +120,10 @@ class Command(BaseCommand):
             )
             product.save()
             product.materials.set(self.get_materials(category, row.material))
-            product.design_details.set(self.get_design_details(category, row.design))
+            try:
+                product.design_details.set(self.get_design_details(category, row.design_details))
+            except:
+                print(row.design_details)
             models.ProductImage.objects.bulk_create([
                 models.ProductImage(product=product, name=name) for name in self.get_images(row.images)
             ])
@@ -123,11 +131,14 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         filename = options['file']
-        df = pd.read_csv(
-            f's3://rossi-rei-data/manufacturers/data/{filename}',
-            encoding='utf-16',
-            sep='\t',
-            engine='python')
+        try:
+            df = pd.read_csv(
+                f's3://rossi-rei-data/manufacturers/data/{filename}',
+                encoding='utf-16',
+                sep='\t',
+                engine='python')
+        except UnicodeError:
+            df = pd.read_csv(f's3://rossi-rei-data/manufacturers/data/{filename}')
         website = self.get_manufacturer_website(df['url'][0])
         manufacturer = self.get_manufacturer(options['manufacturer'], website)
         self._populate(df, manufacturer, options['category'])
