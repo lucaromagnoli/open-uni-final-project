2b039d5afc532c16c3b521b352e8206b80f9d707
Luca Romagnoli <luca.romagnoli@sainsburys.co.uk>
2020-04-16 23:14:15 +0100

    add static to urls

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,6 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
-from django_filters.views import FilterView
 
 from .views import product_list, load_types
 
@@ -8,3 +9,7 @@ urlpatterns = [
     path('products', product_list),
     path('ajax/load-types/', load_types, name='ajax_load_types'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
