75db10544f8662d9305c2f84910125fe7b43b147
Luca Romagnoli <luca.romagnoli@sainsburys.co.uk>
2020-04-16 10:30:14 +0100

    improve filtering

--- products/views.py
+++ products/views.py
@@ -2,25 +2,20 @@ from django.shortcuts import render
 from django.db.models import Q
 import django_filters
 
-from .models import Product
+from .models import Product, CategoryType
 
 
 class ProductFilter(django_filters.FilterSet):
     title = django_filters.CharFilter(lookup_expr='iexact')
+    type = django_filters.ModelChoiceFilter(queryset=CategoryType.objects.none())
 
     class Meta:
         model = Product
-        fields = ['manufacturer', 'gender', 'category', 'type']
+        fields = ('manufacturer', 'gender', 'category', 'type')
 
 
-def index(request):
-    context = {
-        'queryset': 'qs'
-    }
-
-    return render(request, 'bootstrap_form.html', context)
-
-
-def product_list(request):
-    f = ProductFilter(request.GET, queryset=Product.objects.all())
-    return render(request, 'template.html', {'filter': f})
+def load_types(request):
+    print(request)
+    category_id = request.GET.get('category')
+    types = CategoryType.objects.filter(category_id=category_id)
+    return render(request, 'types_dropdown_list_options.html', {'types': types})
