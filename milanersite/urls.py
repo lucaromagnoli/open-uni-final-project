6068e094a8df6e146b1ea8e4265ace089c60ab39
Luca Romagnoli <luca.romagnoli@sainsburys.co.uk>
2020-04-21 18:41:10 +0100

    home to product_list

--- milanersite/urls.py
+++ milanersite/urls.py
@@ -3,9 +3,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 
+from products.views import product_list
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
+    path('', product_list, name='home')
 ]
 
 
