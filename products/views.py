de8184716fe29367e4cdb4b09d2c3e87dd88731a
Luca Romagnoli <luca.romagnoli@sainsburys.co.uk>
2020-04-22 09:37:29 +0100

    unused imprt

--- products/views.py
+++ products/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.db.models import Q
 import django_filters
 
 from .models import Product, CategoryType
@@ -25,7 +24,6 @@ class ProductFilter(django_filters.FilterSet):
 
 
 def load_types(request):
-    print(request)
     category_id = request.GET.get('category')
     types = CategoryType.objects.filter(category_id=category_id)
     return render(request, 'types_dropdown_list_options.html', {'types': types})
