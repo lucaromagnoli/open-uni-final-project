5520114f91b3c457177e3115385c0e207c45610e
Luca Romagnoli <luca.romagnoli@sainsburys.co.uk>
2020-05-06 18:26:52 +0100

    add milaner logo

--- milanersite/urls.py
+++ milanersite/urls.py
@@ -1,11 +1,14 @@
-from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import include, path
 
 from products.views import product_list
 
 urlpatterns = [
+    # path('login/', auth_views.login, name='login'),
+    # path('logout/', auth_views.logout, name='logout'),
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
     path('', product_list, name='home')
