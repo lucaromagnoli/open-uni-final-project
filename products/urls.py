0ffc53511123857d0499876388082147fcfe3058
Luca Romagnoli <luca.romagnoli@sainsburys.co.uk>
2020-08-14 17:04:03 +0100

    feat: use SearchView in urls

--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,13 @@
+from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from .views import product_list, product_detail, load_types
+from .views import SearchView, product_detail, load_types
 
 app_name = 'milaner'
 urlpatterns = [
-    path('products', product_list, name='product_list'),
+    path('products', login_required(SearchView.as_view()), name='product_list'),
     path('products/<int:pk>', product_detail, name='product_detail'),
     path('ajax/load-types/', load_types, name='ajax_load_types'),
 ]
